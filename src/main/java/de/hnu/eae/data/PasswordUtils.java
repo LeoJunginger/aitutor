ed97e0af491262a6ac2ba649d04b997527f2aa40
rsteinborn <ra.steinborn@gmail.com>
2024-01-15 10:07:22 +0100

    Add utility methods for hashing and verifying passwords using BCrypt algorithm

--- src/main/java/de/hnu/eae/data/PasswordUtils.java
+++ src/main/java/de/hnu/eae/data/PasswordUtils.java
@@ -2,12 +2,28 @@ package de.hnu.eae.data;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+/**
+ * This class provides utility methods for hashing and verifying passwords using BCrypt algorithm.
+ */
 public class PasswordUtils {
 
+    /**
+     * Hashes the given plain text password using BCrypt algorithm.
+     *
+     * @param plainTextPassword the plain text password to be hashed
+     * @return the hashed password
+     */
     public static String hashPassword(String plainTextPassword){
         return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
     }
 
+    /**
+     * Verifies the given plain text password against the hashed password using BCrypt algorithm.
+     *
+     * @param plainTextPassword the plain text password to be verified
+     * @param hashedPassword the hashed password to be compared against
+     * @return true if the plain text password matches the hashed password, false otherwise
+     */
     public static boolean verifyUserPassword(String plainTextPassword, String hashedPassword) {
         return BCrypt.checkpw(plainTextPassword, hashedPassword);
     }
