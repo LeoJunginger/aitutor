94d63a30dc3a8d3caae3105f6e0d63a27047081a
rsteinborn <ra.steinborn@gmail.com>
2024-01-16 23:08:59 +0100

    Refactor AuthToken class to include constructor and getter/setter methods

--- src/main/java/de/hnu/eae/util/AuthToken.java
+++ src/main/java/de/hnu/eae/util/AuthToken.java
@@ -1,19 +1,34 @@
 package de.hnu.eae.util;
 
 /**
- * Class to encapsulate the JWT token in a response.
+ * Represents an authentication token.
  */
 public class AuthToken {
     private String token;
 
+    /**
+     * Constructs a new AuthToken object with the specified token.
+     * 
+     * @param token the authentication token
+     */
     public AuthToken(String token) {
         this.token = token;
     }
 
+    /**
+     * Retrieves the authentication token.
+     * 
+     * @return the authentication token
+     */
     public String getToken() {
         return token;
     }
 
+    /**
+     * Sets the authentication token.
+     * 
+     * @param token the authentication token to set
+     */
     public void setToken(String token) {
         this.token = token;
     }
